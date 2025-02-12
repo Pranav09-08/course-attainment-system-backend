09d588f05ccdec7101954a3cb90ff38ebeb8b3f8
Pranav Kiran Mahale <pranav@Pranavs-MacBook-Air.local>
2025-02-12 16:36:20 +0530

    File deleted

--- routes/loginRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const loginController = require("../controllers/login_Controller");
-
-const router = express.Router();
-
-router.post("/login",loginController.login);
-
-module.exports= router;
