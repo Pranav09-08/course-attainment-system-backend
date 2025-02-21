6c99b2a954839898a62bbdcbe146f3bb44440235
Dipali <deepalideore2005@gmail.com>
2025-02-21 22:32:56 +0530

    Courses add, see, and update done

--- routes/admin/courseRoute.js
+++ routes/admin/courseRoute.js
@@ -3,7 +3,9 @@ const router = express.Router();
 const courseController = require('../../controllers/admin/courseController');
 const authenticateToken = require("../../middleware/authLoginMiddleware");
 
-// Route to add a new course
 router.post('/add-course', authenticateToken, courseController.addCourse);
+router.get('/get-courses', authenticateToken, courseController.getCourses);
+router.delete('/delete-course/:course_id', authenticateToken, courseController.deleteCourse);
+router.put('/update-course/:course_id', authenticateToken, courseController.updateCourse);
 
 module.exports = router;
