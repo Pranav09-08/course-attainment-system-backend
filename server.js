bc7b154f5794103522304f1ffbde8402788c97ae
pranav <pranavmahale08@gmail.com>
2025-07-05 22:41:35 +0530

    Server.js updated with Segregation of Routes and Import

--- server.js
+++ server.js
@@ -3,32 +3,39 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const path = require('path');
-// Existing routes
+
+
+// Faculty Imports for Routes
 const courseRoute = require('./routes/faculty/coursesRoute');
 const facultRoute2 = require('./routes/faculty/courseAllotmentRoute');
+const marksRoutes = require('./routes/faculty/courseAllotmentRoute');
+const addmarks =require('./routes/faculty/marksRoute');
+const getStudentRoute =require('./routes/faculty/getStudentRoute');
+const updateMarksRoute = require('./routes/faculty/updateMarksRoute');
+
+
+// Common Imports for Routes
 const authLoginRoute = require("./routes/authLoginRoute");
 const facultyRoute = require('./routes/profileRoute');
 const updateProRoute = require('./routes/updateProfileRoute');
 const dashboardRoutes = require("./routes/dashboardAuth");
+const contactRoute = require("./routes/contactRoute");
+
+
+// Admin Imports for Routes
 const adminRoutes = require('./routes/admin/adminRoute');
-const attainmentRoutes = require('./routes/coordinator/attainmentRoutes');
-const setTarget = require('./routes/coordinator/setTargetRoute');
-const marksRoutes = require('./routes/faculty/courseAllotmentRoute');
 const adminCourseRoute = require('./routes/admin/courseRoute');
 const adminCourseCoordinatorRoute = require('./routes/admin/courseCoordinatorRoute');
-const courseReportRoutes = require('./routes/coordinator/courseReportRouter');
-
-const addmarks =require('./routes/faculty/marksRoute');
 const courseAllotmentRoutes = require("./routes/admin/courseAllotmentRoute");
-const getStudentRoute =require('./routes/faculty/getStudentRoute');
-const updateMarksRoute = require('./routes/faculty/updateMarksRoute');
 const studentRoute = require('./routes/admin/studentRoute');
 const courseAttainmentRoutes = require('./routes/admin/attainmentAnalysis');
 
+// Coordinator Imports for Routes
+const attainmentRoutes = require('./routes/coordinator/attainmentRoutes');
+const setTarget = require('./routes/coordinator/setTargetRoute');
+const courseReportRoutes = require('./routes/coordinator/courseReportRouter');
 
 
-// New contact route
-const contactRoute = require("./routes/contactRoute");
 
 const app = express();
 
@@ -37,31 +44,41 @@ app.use(cors());
 app.use(express.json());
 app.use(bodyParser.json());
 
-// Define API routes
+//Faculty Routes
 app.use("/courses", courseRoute);
 app.use("/faculty_courses", facultRoute2);
-app.use("/attainment", attainmentRoutes);
-app.use("/report",courseReportRoutes);
-app.use("/set_target", setTarget);
+app.use('/marks', marksRoutes);
+app.use('/add_marks',addmarks);
+app.use("/get_student",getStudentRoute);
+app.use("/update", updateMarksRoute);
+
+
+
+//Common Routes
 app.use("/auth", authLoginRoute);
 app.use("/profile", facultyRoute);
 app.use("/dashboard", dashboardRoutes);
+app.use('/api/profile', updateProRoute);
+app.use('/upload_image', express.static(path.join(__dirname, 'upload_image')));
+app.use("/contact", contactRoute);
+
+
+//Coordinator Routes
+app.use("/attainment", attainmentRoutes);
+app.use("/report",courseReportRoutes);
+app.use("/set_target", setTarget);
+
+
+//Admin Routes
 app.use("/admin", adminRoutes);
 app.use("/admin/course", adminCourseRoute);
 app.use("/admin/allotment", courseAllotmentRoutes);
 app.use("/admin/coordinator",adminCourseCoordinatorRoute);
 app.use("/admin/student",studentRoute);
 app.use('/admin/course-attainment-analysis',courseAttainmentRoutes);
-//app.use('/update',updateProRoute);
-app.use('/api/profile', updateProRoute);
-app.use('/upload_image', express.static(path.join(__dirname, 'upload_image')));
 
-// New contact route
-app.use("/contact", contactRoute);
-app.use('/marks', marksRoutes);
-app.use('/add_marks',addmarks);
-app.use("/get_student",getStudentRoute);
-app.use("/update", updateMarksRoute);
+
+
 // Example route for testing
 app.get("/", (req, res) => {
   res.send("Backend server is running!");
