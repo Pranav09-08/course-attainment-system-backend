6d08e1cd18f39c491a70f86e5b9c371723b0aabc
pranav <pranavmahale08@gmail.com>
2025-07-16 19:32:56 +0530

    Authentication Token added to routes

--- routes/admin/studentRoute.js
+++ routes/admin/studentRoute.js
@@ -7,18 +7,19 @@ const {
   deleteStudentController 
 } = require('../../controllers/admin/studentController');
 const multer = require('multer');
+const authenticateToken = require("../../middleware/authLoginMiddleware");
 const upload = multer({ storage: multer.memoryStorage() });
 
 // Upload students (now expects sem and academic_yr in body)
-router.post('/upload-students', upload.single('file'), uploadStudents);
+router.post('/upload-students', authenticateToken,upload.single('file'), uploadStudents);
 
 // Get students (now requires sem and academic_yr as query params)
-router.get('/get-students', getStudents);
+router.get('/get-students', authenticateToken,getStudents);
 
 // Update student (now requires sem and academic_yr in body)
-router.put('/update-student/:roll_no', updateStudentController);
+router.put('/update-student/:roll_no', authenticateToken, updateStudentController);
 
 // Delete student (now requires sem and academic_yr as query params)
-router.delete('/delete-student/:roll_no', deleteStudentController);
+router.delete('/delete-student/:roll_no', authenticateToken,deleteStudentController);
 
 module.exports = router;
--- routes/coordinator/courseReportRouter.js
+++ routes/coordinator/courseReportRouter.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const courseReportController = require('../../controllers/coordinator/coursereportController');
+const authenticateToken = require("../../middleware/authLoginMiddleware");
 
 // Route for generating and downloading the report
-router.get('/download-report', courseReportController.downladReport);
-router.get('/generate-report', courseReportController.generateReport);
-router.get('/show-marktarget', courseReportController.getmarksTarget);
+router.get('/download-report', authenticateToken,courseReportController.downladReport);
+router.get('/generate-report', authenticateToken,courseReportController.generateReport);
+router.get('/show-marktarget', authenticateToken,courseReportController.getmarksTarget);
 
 module.exports = router;
